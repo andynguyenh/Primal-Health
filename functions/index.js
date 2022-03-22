659194f93d8eb57c573679ab89e0aca4f8a90955
Austin Pakola <austinpakola@gmail.com>
2022-03-21 19:38:15 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -28,16 +28,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDPK0DfxvHKHLCqmQPMXEV-PvBeF5spejw",
-  authDomain: "primal-health.firebaseapp.com",
-  databaseURL: "https://primal-health-default-rtdb.firebaseio.com",
-  projectId: "primal-health",
-  storageBucket: "primal-health.appspot.com",
-  messagingSenderId: "978802298358",
-  appId: "1:978802298358:web:57980813be26ffa6cf8bd7",
-  measurementId: "G-6KEVHP9CHQ"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
